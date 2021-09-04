7cec1312c9fdde1bdcaf98f10b212c3f68168187
Hyup <kkddong98@gmail.com>
2021-09-05 00:49:32 +0900

    no message

--- Genetic Algorithm simulation/src/Gene.java
+++ Genetic Algorithm simulation/src/Gene.java
@@ -2,7 +2,11 @@ public class Gene {
     private final int speed;
     //개체 크기
     private final int radius;
+    //onewMove가 호출 시 move함수 호출 전 휴식 기간
     private final int activity;
+    //이동시 직진하는 빈도 수->n번 직진 후 방향수정
+    private final int straightness;
+    private final double rateOfMtation = 0.01;
 
     //Prey개체가 번식시 사용
     public Gene(int speed, int radius, int activity) {
@@ -21,7 +25,7 @@ public class Gene {
 
     //자기 유전자를 바탕으로 자식 유전자 생성 ->돌연변이 등등 고려
     public Gene Genetic() {
-        //어떻게 할지 잘 생각해보자
+
         return new Gene();
     }
 
